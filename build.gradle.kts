550d0bb2df453facc679537f762901aa3bb425ed
Timofey Ivankov <playboicartina@gmail.com>
2025-11-16 21:49:15 +0300

    updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 dependencies {
     implementation("jakarta.jms:jakarta.jms-api:3.1.0")
     implementation("org.apache.activemq:activemq-broker:6.1.1")
+    implementation("com.rabbitmq:amqp-client:5.27.1")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
     testImplementation(kotlin("test"))
 }
 
