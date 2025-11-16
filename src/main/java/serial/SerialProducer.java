7f6fe890d603c386bab84aba32a1c46e42483dca
Timofey Ivankov <playboicartina@gmail.com>
2025-11-16 23:00:30 +0300

    producer

--- src/main/java/serial/SerialProducer.java
+++ src/main/java/serial/SerialProducer.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.TimeoutException;
 
+import com.rabbitmq.client.ShutdownSignalException;
 import shared.Producer;
 
 public class SerialProducer {
@@ -20,19 +21,17 @@ public class SerialProducer {
             IOException,
             TimeoutException
     {
-//        String[] pathArray = {
-//                "src/main/java/data/data_100MB",
-//                "src/main/java/data/data_200MB",
-//                "src/main/java/data/data_500MB",
-//                "src/main/java/data/data_1GB",
-//                "src/main/java/data/data_1p5_GB"
-//        };
-//        for (String path : pathArray) {
-//            String content = new String(Files.readAllBytes(Path.of(path)));
-//            this.messageProdcuer.produceMessage(content);
-//        }
-        String content = "content";
-        this.messageProdcuer.produceMessage(content);
+        String[] pathArray = {
+                "src/main/java/data/data_100MB",
+                "src/main/java/data/data_200MB",
+                "src/main/java/data/data_500MB",
+                "src/main/java/data/data_1GB",
+        };
+        for (String s : pathArray) {
+            System.out.println("Producing " + s);
+            String content = new String(Files.readAllBytes(Path.of(s)));
+            this.messageProdcuer.produceMessage(content);
+        }
         this.messageProdcuer.closeConnection();
     }
 }
