9355ec00d56b116c3fa2cfee0a450efc8e41e2d6
Timofey Ivankov <playboicartina@gmail.com>
2025-11-22 22:43:47 +0300

    agg consumer

---
+++ src_py/aggregation_consumer/aggregation/aggregation.py
@@ -0,0 +1,49 @@
+import asyncio
+import time
+
+from aio_pika.abc import (
+    AbstractRobustConnection, 
+    AbstractQueue, 
+    AbstractMessage,
+)
+
+from client import MessageClient
+from serial_consumer.consumer import SerialConsumer
+
+
+class AggregationConsumer:
+    
+    __slots__ = (
+        '_consumer',
+        '_client',
+    )
+    
+    def __init__(
+        self,
+        consumer: SerialConsumer = SerialConsumer(),
+        client: MessageClient = MessageClient(),
+    ) -> None:
+        
+        self._consumer = consumer
+        self._client = client
+        
+    async def consume(self) -> None:
+        message: AbstractMessage
+        connection: AbstractRobustConnection = (
+            await self._client.get_connection()
+        )
+        async with connection:
+            queue: AbstractQueue = await self._client.get_queue(
+                connection, 
+                'message_exchange'
+            )
+            async with queue.iterator() as queue_iter:
+                async for message in queue_iter:
+                    await self._consumer.consume(message)
+                    await message.ack()
+                    if queue.name in message.body.decode():
+                        break
+                    
+if __name__ == '__main__':
+    collector = AggregationConsumer()
+    asyncio.run(collector.consume())
